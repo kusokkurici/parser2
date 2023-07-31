abdb52a29bb269d6e4b0eb8541aecf98aff7b38a
Yaroslav Galiulin <yaroslav66@list.ru>
2023-07-31 17:38:43 +0500

    Initial commit

---
+++ get_email.py
@@ -0,0 +1,55 @@
+import re
+import requests
+
+from list_from_exel import read_website_column
+
+#  данные для подключения к файлу эксель
+file_path = "2gispars.xlsx"  # Укажите путь к вашему файлу Excel
+sheet_name = "Sheet1"       # Укажите имя листа, на котором находится столбец
+column_name = "Веб-сайт 1"  # Укажите название столбца
+
+
+def load_email():
+    """ Функция собирает имейли и записывает их в файл output.txt"""
+    urls_all = read_website_column(file_path, sheet_name, column_name)
+    email_list = []
+    for url in urls_all:
+        email_check = find_emails_on_website(url)  # Список с имейлами найдеными на сайтах
+
+        if email_check:  # Если список не пустой то
+            for email in email_check:  # перебираем список
+                if email not in email_list:  # если значение нет в готовом списке мейлов, то
+                    email_list.append(email)  # добавляем имей в готовый список
+
+    with open('output.txt', 'w') as file:
+        for item in email_list:
+            file.write(str(item) + '\n')
+
+    return print('Operation complete, check file output.txt')
+
+
+def find_emails_on_website(url):
+    """функция поиска имейлов на url страницах"""
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        # Получаем содержимое страницы сайта
+        response = requests.get(url, headers=headers)
+        # Получаем содержимое страницы сайта
+        if response.status_code != 200:
+            print(f"Не удалось получить содержимое страницы {url}. Код ответа: {response.status_code}")
+            print('Рекомендуем удалить сайт из поиска')
+            return []
+
+        # Определяем регулярное выражение для поиска адресов электронной почты
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+
+        # Находим все совпадения с регулярным выражением в содержимом страницы
+        matches = re.findall(email_pattern, response.text)
+
+        return matches
+    # обработка ошибки
+    except Exception as e:
+        print(f"Произошла ошибка на сайте {url} при обращении к сайту: {e}")
+        print('Рекомендуем удалить сайт из поиска')
+        return []
---
+++ list_from_exel.py
@@ -0,0 +1,28 @@
+import openpyxl
+
+
+def read_website_column(file_path, sheet_name, column_name):
+    """Функция считывает данные из файла эксель, возвращает список данных из заданного столбика"""
+    website_list = []
+
+    # Открываем файл Excel
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook[sheet_name]
+
+    # Находим номер столбца "Веб-сайт 1" по его названию
+    column_index = None
+    for col_num, cell in enumerate(sheet.iter_cols(min_col=1, max_col=sheet.max_column, values_only=True)):
+        if cell[0] == column_name:
+            column_index = col_num + 1
+            break
+
+    if column_index is None:
+        return print(f"Столбец '{column_name}' не найден в файле.")
+
+    # Считываем значения из столбца "Веб-сайт 1" в список, пропуская пустые строки
+    for row_num, cell_value in enumerate(sheet.iter_cols(min_col=column_index, max_col=column_index, values_only=True)):
+        list_url = list(cell_value)
+        for i in list_url[1:]:
+            if i is not None:
+                website_list.append(i)
+    return website_list
